3581c8f749240ae30671cde77d2022969850ad78
Mathias Meyer <meyer@paperplanes.de>
2011-06-22 02:48:24 -0700

    Allow iframe elements for embedded videos.

--- lib/gollum-site/sanitization.rb
+++ lib/gollum-site/sanitization.rb
@@ -10,7 +10,7 @@ module Gollum
       'blockquote', 'br', 'button', 'caption', 'center', 'cite',
       'code', 'col', 'colgroup', 'dd', 'del', 'dfn', 'dir',
       'div', 'dl', 'dt', 'em', 'fieldset', 'font', 'form', 'h1',
-      'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'i', 'img', 'input',
+      'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'i', 'iframe', 'img', 'input',
       'ins', 'kbd', 'label', 'legend', 'li', 'map', 'menu',
       'ol', 'optgroup', 'option', 'p', 'pre', 'q', 's', 'samp',
       'select', 'small', 'span', 'strike', 'strong', 'sub',
@@ -22,6 +22,7 @@ module Gollum
     ATTRIBUTES = {
       'a'   => ['href'],
       'img' => ['src'],
+      'iframe' => ['src'],
       :all  => ['abbr', 'accept', 'accept-charset',
                 'accesskey', 'action', 'align', 'alt', 'axis',
                 'border', 'cellpadding', 'cellspacing', 'char',
